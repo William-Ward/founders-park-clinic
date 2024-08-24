fc9729a286dea974efe47d32b259e99412caf8ae
William Ward <ward082@direct-automation.com>
2024-08-24 12:51:05 -0500

    initial commit

---
+++ create_payment.php
@@ -0,0 +1,47 @@
+<?php
+    require 'vendor/autoload.php';
+
+    use Square\SquareClient;
+
+    // get amount and invoice id from the form
+    $amount = $_POST['amount'];
+    $invoice_id = $_POST['invoice-id'];
+
+
+    $client = new SquareClient([
+        'accessToken' => $prod_access_token,
+        'environment' => \Square\Environment::PRODUCTION
+    ]);
+
+    $price_money = new \Square\Models\Money();
+    $price_money->setAmount($amount);
+    $price_money->setCurrency('USD');
+    
+    $quick_pay = new \Square\Models\QuickPay(
+        'test',
+        $price_money,
+        $prod_location_id
+    );
+    
+    $body = new \Square\Models\CreatePaymentLinkRequest();
+    $body->setQuickPay($quick_pay);
+    
+    $api_response = $client->getCheckoutApi()->createPaymentLink($body);
+    
+    if ($api_response->isSuccess()) {
+        $result = $api_response->getResult();
+        $payment_link = $result->getPaymentLink()->getUrl();
+    } else {
+        $errors = $api_response->getErrors();
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Test</title>
+</head>
+<body>
+    <button onclick="window.location.href='<?php echo $payment_link; ?>'">Pay</button>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Test</title>
+</head>
+<body>
+    <form action="create_payment.php" method="post">
+        <label for="amount">Amount:</label><br>
+        <input type="text" id="amount" name="amount" value="100"><br>
+        <label for="invoice-id">invoice ID:</label><br>
+        <input type="text" id="invoice-id" name="invoice-id"><br><br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
