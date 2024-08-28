925bc5ebbfc6770f93ec6140e9a828a0a94606b3
William Ward <ward082@direct-automation.com>
2024-08-27 20:59:08 -0500

    add composer step to workflow

--- create_payment.php
+++ create_payment.php
@@ -6,12 +6,15 @@
     // get amount and invoice id from the form
     $amount = $_POST['amount'];
     $invoice_id = $_POST['invoice-id'];
-    $square_token = getenv('SQUARE_TOKEN');
-    $loaction_id = getenv('LOCATION_ID');
+    // output the square token to the console
+    $square_token = $_ENV['SQUARE_TOKEN'] || die ('Square token not found');
+    $loaction_id = $_ENV['LOCATION_ID'] || die ('Location ID not found');
+    
+    die ($square_token);
 
     $client = new SquareClient([
         'accessToken' => $square_token,
-        'environment' => \Square\Environment::PRODUCTION
+        'environment' => \Square\Environment::SANDBOX
     ]);
 
     $price_money = new \Square\Models\Money();
